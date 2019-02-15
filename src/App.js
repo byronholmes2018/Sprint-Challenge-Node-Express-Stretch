10385bf5c6289beb48bcdc141e009e386d4c1f75
byronholmes2018 <lshopeful2018@mgmail.com>
2019-02-15 12:40:52 -0600

    Finished getting projects and actions from api and displaying them to screen.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+
+let id = null
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      fetching: true,
+      projects:[],
+      id: id,
+      actions: []
+    }
+  }
+  componentDidMount(){
+    axios.get('http://localhost:3000/api/projects')
+      .then(response=>{
+        console.log(response.data);
+        this.setState(prevState=>({projects: response.data, fetching:false}))
+      })
+      .catch(err=>console.log(err))
+  }
+  handleClick = (id)=>{
+    axios.get(`http://localhost:3000/api/actions/${id}`)
+      .then(action=>{console.log(action); this.setState({actions: action.data})})
+      .catch(err=>console.log(err))
+  }
   render() {
+
     return (
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         {this.state.fetching ? (<h3>Hold on, loading data</h3>) :(<div>{this.state.projects.map((project,index)=>{
+            return <div onClick = {()=>this.handleClick(project.id)}>
+                      <div>{project.name}</div><div>{project.description}</div>
+                      <div className = "description-class">{this.state.actions.description}</div>
+                    </div>
+         })}</div>)}
       </div>
     );
   }
